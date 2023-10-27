d079172ec2a6e49706f3591cb9dc6cbfa134def4
Kinsley Alexander <kinsleyalexander@Kinsleys-MacBook-Air.local>
2023-10-27 17:38:33 -0400

    Commiting changes made previously

--- src/Components/AlbumView.js
+++ src/Components/AlbumView.js
@@ -1,5 +1,3 @@
-// These components will be making separate API calls from the app
-// component to serve specific data about a given album
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
@@ -7,11 +5,30 @@ function AlbumView() {
     const { id } = useParams()
     const [ albumData, setAlbumData ] = useState([])
 
+    useEffect(() => {
+        const fetchData = async () => {
+            const API_URL = `http://localhost:4000/song/${id}`
+            const response = await fetch(API_URL)
+            const data = await response.json()
+            const songs = data.results.filter(item => item.kind === 'song')
+            setAlbumData(songs)
+        }
+
+        fetchData()
+    })
+
+    const display = albumData && albumData.map(song => {
+        return (
+            <div key={song.trackId}>
+                <p>{song.trackName}</p>
+            </div>
+        )
+    })
+
     return (
-        <div>
-            <h2>The id passed was: {id}</h2>
-            <p>Album Data Goes Here!</p>
-        </div>
+        <>
+            {display}
+        </>
     )
 }
 
